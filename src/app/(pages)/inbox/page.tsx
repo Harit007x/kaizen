6358a59d766a6547c38a0f52b3927841e69ea1a1
Harit007x <harit.ptl.business@gmail.com>
2024-11-01 23:07:04 +0530

    implemented two pragmatic examples

---
+++ src/app/(pages)/inbox/page.tsx
@@ -0,0 +1,12 @@
+import BoardExample from '@/components/board-dnd/example'
+import { List } from '@/components/single-list-dnd/list'
+import React from 'react'
+
+export default function InboxPage() {
+    return (
+    <div>
+        {/* <List /> */}
+        <BoardExample/>
+    </div>
+  )
+}
